ef7c5e44a508ccf654a08edecbaa0e704ef596c7
Oosh74 <usherjared@yahoo.com>
2024-10-02 20:20:49 -0400

    Add new button to draw new grid and initial logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('.grid-container');
+const changeGridBtn = document.querySelector('.change-grid-btn');
 
 const drawGrid = () => {
   for (let i = 0; i < 256; i++) {
@@ -8,5 +9,27 @@ const drawGrid = () => {
     console.log(i);
   }
 };
+
+changeGridBtn.addEventListener('click', () => {
+  let gridNumber = prompt(
+    'How large should the grid be (must be 100x100 or smaller)?'
+  );
+
+  while (gridNumber > 100 || isNaN(gridNumber)) {
+    if (gridNumber > 100) {
+      gridNumber = prompt(
+        'The grid cannot be larger than 100x100, please input a smaller number'
+      );
+    } else if (isNaN(gridNumber)) {
+      gridNumber = prompt(
+        'You did not enter a valid number, please try again.'
+      );
+    } else {
+      break;
+    }
+  }
+
+  return console.log('gridNumber', gridNumber);
+});
 
 drawGrid();
